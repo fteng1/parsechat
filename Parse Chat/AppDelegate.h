d88c1af92305120a1ce40ab0730b27ea1e37bf72
Felianne Teng <felianneteng@fb.com>
2021-07-06 08:36:31 -0700

    Initial Commit

---
+++ Parse Chat/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Parse Chat
+//
+//  Created by Felianne Teng on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Parse Chat/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Parse Chat
+//
+//  Created by Felianne Teng on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
